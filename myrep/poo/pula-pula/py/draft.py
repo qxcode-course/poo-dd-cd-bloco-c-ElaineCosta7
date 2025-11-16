2d05b317124d67d226bfeb1c12b467c22f603124
ElaineCosta7 <elainetiny616@gmail.com>
2025-11-16 22:42:49 +0000

    continuando trampolim

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -23,18 +23,46 @@ class Trampolim:
 
     def arrive(self, criança: Criança) -> None:
         self.__espera.append(criança)
-        self.__espera.sort(key=lambda c: c.getIdade())
 
     def enter(self):
-        if len(self.__espera) == 0:
+        if len(self.__espera) > 0:
+            aux: Criança = self.__espera.pop(0)
+            self.__brincando.append(aux)
             return
-        criança = self.__espera.pop(-1)
-        self.__brincando.append(criança)
+        
+    def leave(self) -> None:
+        if len(self.__espera) > 0:
+            aux: Criança = self.__espera.pop(0)
+            self.__espera.append(aux)
+            return
+        
+    def removeList(self, criança:Criança, nome:str, idade:int):
+        for i, criança in enumerate(self.__espera):
+            if criança.getNome() == nome and criança.getIdade() == idade:
+                del self.__espera[i]
+                return
+            
+        for i, criança in enumerate(self.__brincando):
+            if criança.getNome() == nome and criança.getIdade() == idade:
+               del self.__espera[i]
+               return
+            
+    def removeKid(self, nome: str) -> Criança | None:   
+        for i, criança in enumerate(self.__espera):
+            if criança.getNome() == nome:
+                del self.__espera[i]
+                return
 
+        for i, criança in enumerate(self.__brincando):
+            if criança.getNome() == nome:
+                del self.__brincando[i]
+                return
+            
+            print("fail: {nome} nao esta no pula-pula")
 
     def __str__(self) -> str:
-        brincando = ", ".join([str(x) for x in self.__brincando])
-        espera = ", ".join([str(x) for x in self.__espera])
+        espera = ", ".join(str(criança) for criança in reversed (self.__espera))
+        brincando = ", ".join(str(criança) for criança in reversed (self.__brincando))
         return f"[{espera}] => [{brincando}]"
 
 def main():
@@ -54,6 +82,11 @@ def main():
             trampolim.arrive(criança)
         elif args[0] == "enter":
             trampolim.enter()
+        elif args[0] == "leave":
+            trampolim.leave()
+        elif args[0] == "remove":
+            nome = args[1]
+            trampolim.removeKid(nome)
         else:
             print("comando invalido")
 
